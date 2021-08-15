d76d7613783156abe9fdf8e4a8041a2eb2f4b4ea
Nikolay Galko <nikolay.galko@gmail.com>
2021-08-15 23:10:28 +0300

    Updated to nests 8, added basic example of version tag in uri

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,20 @@
-import {Body, Controller, Get, Post} from '@nestjs/common';
+import {Body, Controller, Get, Post, Version} from '@nestjs/common';
 import { AppService } from './app.service';
 import {CreateCatDto} from "./dto/exampleDTO.service";
 
-@Controller()
+@Controller({
+  version: '1',
+})
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Version('1')
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
+  @Version('2')
   @Post('example')
   async example(@Body() createCatDto: CreateCatDto): Promise<boolean> {
     return true;
--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,31 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
 import * as helmet from 'helmet';
 import * as csurf from 'csurf';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { VersioningType } from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  app.use(csurf());
-  app.enableCors();
-  app.use(helmet());
+    //app.use(csurf());
+    app.enableCors();
+    app.use(helmet());
+    app.enableVersioning({
+        type: VersioningType.URI
+    });
 
-  const config = new DocumentBuilder()
-      .setTitle('Galek example of http microservice')
-      .setDescription('The API description of example of http microservice')
-      .setVersion('1.0')
-      .addTag('Galek micro-service')
-      .build();
+    const config = new DocumentBuilder()
+        .setTitle('Galek example of http microservice')
+        .setDescription('The API description of example of http microservice')
+        .setVersion('1.0')
+        .addTag('Galek micro-service')
+        .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
 
-  await app.listen(process?.env?.PORT ?? 3000);
+    await app.listen(process?.env?.PORT ?? 3000);
 }
+
 bootstrap();
